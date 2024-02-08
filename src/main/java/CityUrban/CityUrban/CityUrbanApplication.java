1525f4d5c1bbc497048bc9f2573fcc6e18dcb3f4
Mailoon <marlongomezm@ielacandelariamedellin.edu.co>
2024-02-08 10:10:51 -0500

    Comienzo

---
+++ src/main/java/CityUrban/CityUrban/CityUrbanApplication.java
@@ -0,0 +1,13 @@
+package CityUrban.CityUrban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityUrbanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityUrbanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/CityUrban/CityUrban/CityUrbanApplicationTests.java
@@ -0,0 +1,13 @@
+package CityUrban.CityUrban;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityUrbanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
